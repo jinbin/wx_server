06b771528b1e24706b0162bc11ba97a9411aea2e
root <root@iZ23y6s3vp3Z.(none)>
2016-01-18 10:42:54 +0800

    接入微信公众号平台

--- application.rb
+++ application.rb
@@ -6,25 +6,30 @@ class Application < Grape::API
 
   Token = "wxtoken18658863627"
 
-  resource :wx do
+  get '/' do 
+    signature = params[:signature]
+    timestamp = params[:timestamp]
+    nonce = params[:nonce]
+    echostr = params[:echostr]
 
-    get '/' do 
-      signature = params[:signature]
-      timestamp = params[:timestamp]
-      nonce = params[:nonce]
-      echostr = params[:echostr]
+    puts "signature: #{signature}"
+    puts "timestamp: #{timestamp}"
+    puts "nonce: #{nonce}"
+    puts "echostr: #{echostr}"
 
-      # 将Token, timestamp, nonce进行字典序排序
-      array = [Token, timestamp, nonce].sort
-      # 将三个参数字符串拼接成一个字符串进行sha1加密, 与signature进行比较
-
-      if signature == Digest::SHA1.hexdigest(array.join)
-        return echostr
-      else 
-        "error/n"
-      end
+    if signature.nil? || timestamp.nil? || nonce.nil? || echostr.nil?
+      return "<p>error</p>"
     end
 
+    # 将Token, timestamp, nonce进行字典序排序
+    array = [Token, timestamp, nonce].sort
+    # 将三个参数字符串拼接成一个字符串进行sha1加密, 与signature进行比较
+
+    if signature == Digest::SHA1.hexdigest(array.join)
+      return echostr
+    else 
+      return "<p>error</p>"
+    end
   end
 
   get "/test" do
